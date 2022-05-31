086cb91dc57e8b758b0cfb2850db55dc0f7f2554
William Gilicinski <william@Marks-MacBook-Pro-2.local>
2022-05-31 11:03:29 -0400

    Starting adding name support

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const {
+  Server
+} = require("socket.io");
 const io = new Server(server);
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/message", function(req, res){
+app.get("/message", function(req, res) {
   res.sendFile(__dirname + "/message.html")
 });
 
@@ -29,11 +31,17 @@ io.on('connection', (socket) => {
 });
 
 io.on('connection', (socket) => {
+  socket.on('name', (name) => {
+    console.log('User: ' + name);
+  });
+});
+
+io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 });
 
-server.listen(3000, function(){
+server.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
